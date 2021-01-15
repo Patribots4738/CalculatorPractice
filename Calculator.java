8dc26e9a9aa2bca5899742e785b17c96e7218cc4
benjaminBroudy <55467400+benjaminBroudy@users.noreply.github.com>
2021-01-14 18:01:51 -0800

    Make Calculator, Not Done
    Still make it so it can do it over and over

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,148 @@ import java.util.Scanner;
 
 public class Calculator {
 
-    Scanner input = new Scanner(System.in);
+    static Scanner input = new Scanner(System.in);
     
+    enum Functions {
+
+        ADD,
+        SUBTRACT,
+        MULTIPLY,
+        DIVIDE
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("WELCOME TO THE ANGRY CALCULATOR REEEEEEEEEEEEEEE");
+
+        while (true) {
+
+            Functions functions = getFunction();
+
+            double[] nums = getNums();
+
+            double output = math(functions, nums);
+
+            System.out.println ("YOUR OUTPUT IS " + output + "     GOOD DAY TO YOU!!!!!!!!!");
+
+        }
+
+    }
+
+    public static double math(Functions functions, double[] arr) {
+
+        double output = 0;
+
+        switch (functions) {
+
+            case ADD:
+                output = arr[0] + arr[1];
+            case SUBTRACT:
+                output = arr[0] - arr[1];
+            case MULTIPLY:
+                output = arr[0] * arr[1];
+            case DIVIDE:
+                output = arr[0] / arr[1];
+
+        }
+
+        return output;
+
+    }
+
+    public static double[] getNums() {
+
+        double[] arr = new double[2];
+
+        boolean running = true;
+
+        while (running) {
+
+            try {
+
+                System.out.println("GIVE ME A NUMBER!!!!!!");
+
+                arr[0] = Double.parseDouble(input.nextLine());
+
+                running = false;
+
+            } catch (Exception e) {
+
+                System.out.println("BRUH. THAT IS NOT A NUMBER.");
+
+            }
+
+        }
+
+        running = true;
+
+        while (running) {
+
+            try {
+
+                System.out.println("GIVE ME ANOTHER NUMBER!!!!!!");
+
+                arr[1] = Double.parseDouble(input.nextLine());
+
+                running = false;
+
+            } catch (Exception e) {
+
+                System.out.println("BRUH. THAT IS NOT A NUMBER.");
+
+            }
+
+        }
+
+        return arr;
+
+    }
+
+    public static Functions getFunction() {
+
+        Functions functions = Functions.ADD;
+
+        String input;
+
+        boolean running = true;
+
+        while (running) {
+
+            System.out.println("WHAT FUNCTION DO YOU WANT TO USE!!!!! (\"A\", \"S\", \"M\", or \"D\")");
+
+            input = Calculator.input.nextLine();
+
+            if (input.equals("A")) {
+
+                functions = Functions.ADD;
+                running = false;
+
+            } else if (input.equals("S")) {
+
+                functions = Functions.SUBTRACT;
+                running = false;
+
+            } else if (input.equals("M")) {
+
+                functions = Functions.MULTIPLY;
+                running = false;
+
+            } else if (input.equals("D")) {
+
+                functions = Functions.DIVIDE;
+                running = false;
+
+            } else {
+
+                System.out.println("Yo. You entered in not a real thing to do. Do it again. Or else...");
+
+            }
+
+        }
+
+        return functions;
+
+    }
+
 }
