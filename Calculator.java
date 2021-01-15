46a59f973f1995ae9877d85ecead2292257a7fe2
Wynter-Tau <33078460+Wynter-Tau@users.noreply.github.com>
2021-01-14 17:46:28 -0800

    Add operation loop, math functions

--- Calculator.java
+++ Calculator.java
@@ -6,7 +6,102 @@ public class Calculator {
 
     public static void main(String[]... args) {
 
+        System.out.println("Please input your operation, your options are: +, -, *, /");
+
+        String operationInput = input.nextLine();
+
+        while(!isValidInput(operationInput, 'o')) {
+
+            System.out.println("Invalid operation, your options are: +, -, *, /");
+
+            operationInput = input.nextLine();
+            
+        }
         
+
+    }
+
+    public static boolean isValidInput(String input, char type) {
+
+        boolean isValid = false;
+
+        switch(type) {
+
+            case 'o':
+
+            if(input.length() == 1) {
+
+                switch(input.charAt(0) {
+
+                    case '+':
+
+                        isValid = true;
+
+                    break;
+
+                    case '-':
+
+                        isValid = true;
+
+                    break;
+
+                    case '*':
+
+                        isValid = true;
+
+                    break;
+
+                    case '/':
+
+                        isValid = true;
+
+                    break;
+
+                }
+
+            }
+
+            break;
+
+            case 'n':
+
+            try {
+
+                Double.parseDouble(input);
+
+                isValid = true;
+
+            } catch(Exception e) {}
+
+            break;
+
+        }
+
+        return isValid;
+
+    }
+
+    public static double add(double num1, double num2) {
+
+        return num1 + num2;
+
+    }
+
+    public static double subtract(double subtrahend, double minuend) {
+
+        return subtrahend - minuend;
+
+    }
+
+    public static double multiply(double num1, double num2) {
+
+        return num1 * num2;
+
+    }
+
+    public static double divide(double dividend, double divisor) {
+
+        return dividend / divisor;
 
     }
     
