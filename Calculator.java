58c606a991d42c0dd49e5a456e0e113be33d5f22
Charlie Havens <charliehavens06@gmail.com>
2021-01-16 14:51:55 -0800

    Make four Operations

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,55 @@
 import java.util.Scanner;
 
-public class Calculator {
+public class Calculator 
+{
 
-    Scanner input = new Scanner(System.in);
     
+    Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+
+        
+    }
+
+    public static double add(double number1, double number2){
+
+       return number1 + number2;
+
+    }
+   
+    public static double subtract(double number1, double number2){
+
+       return number1 - number2;
+       
+    }
+
+    public static double multiply(double number1, double number2){
+
+       return number1 * number2;
+       
+    }
+
+    public static double divide(double number1, double number2){
+
+       return number1 / number2;
+       
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
