b8bcd4af7101842e2bbed3779fe1d4a0cf235a03
Jacob1010-h <lordgaming1010@gmail.com>
2021-01-16 16:08:56 -0800

    Make nearly entire program without division

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,34 @@
 import java.util.Scanner;
 
-public class Calculator {
+public class Calculator 
+{
+    static Scanner scanner = new Scanner(System.in);
 
-    Scanner input = new Scanner(System.in);
+public static void main(String[] args) {
+
+    while(true){
+        System.out.println("Enter your operation.");
+        String calculatorString = scanner.nextLine(); 
+
+        switch (calculatorString) {
+            case "+":
+                Functions.add();
+                
+                break;
     
+            case "-":
+                Functions.minus();
+    
+                break;
+    
+            case "*":
+            Functions.muitiply();
+    
+                break; 
+        } 
+    
+    }
+   
+
+}
 }
---
+++ Functions.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+public class Functions{
+
+   static Scanner scanner;
+// Add Function 
+    
+    public static void add(){
+ 
+        try
+         {
+            scanner = new Scanner(System.in); 
+            double firstNum, secondNum, answer; 
+
+            System.out.println("Enter your first number");
+            firstNum = scanner.nextDouble(); 
+
+            System.out.println("Enter your second number: ");
+           secondNum = scanner.nextDouble();
+
+           answer = firstNum + secondNum;
+           System.out.println(answer);
+         }
+            catch (Exception e)
+         {
+                System.out.println("Invalid input");
+          }
+            
+    }
+
+
+// Minus Function
+   public static void minus(){
+ 
+        try
+         {
+            scanner = new Scanner(System.in); 
+            double firstNum, secondNum, answer; 
+
+            System.out.println("Enter your first number");
+            firstNum = scanner.nextDouble(); 
+
+            System.out.println("Enter your second number: ");
+           secondNum = scanner.nextDouble();
+
+           answer = firstNum - secondNum;
+           System.out.println(answer);
+         }
+            catch (Exception e)
+         {
+                System.out.println("Invalid input");
+          }
+         
+    }
+
+
+// Multiply Function
+    public static void muitiply(){
+ 
+        try
+         {
+            scanner = new Scanner(System.in); 
+            double firstNum, secondNum, answer; 
+
+            System.out.println("Enter your first number");
+            firstNum = scanner.nextDouble(); 
+
+            System.out.println("Enter your second number: ");
+           secondNum = scanner.nextDouble();
+
+           answer = firstNum * secondNum;
+           System.out.println(answer);
+         }
+            catch (Exception e)
+         {
+                System.out.println("Invalid input");
+          }
+           
+    }
+   }
